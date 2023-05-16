e0778363327931e1ce91624ebd7283039a1e0918
Seungheon LEE <317tmdgjs@naver.com>
2023-05-16 15:37:35 +0900

    [Silver II] Title: 촌수계산, Time: 216 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -0,0 +1,56 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    static ArrayList<ArrayList<Integer>> map = new ArrayList<>();
+    static int one;
+    static int two;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            map.add(new ArrayList<>());
+        }
+
+        one = sc.nextInt();
+        two = sc.nextInt();
+
+        int M = sc.nextInt();
+        for (int num = 0; num < M; num++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+
+            map.get(start).add(end);
+            map.get(end).add(start);
+        }
+
+        System.out.println(solve(one, 0));
+    }
+
+    public static int solve(int cur, int count) {
+
+        if (cur == two) {
+            return count;
+        }
+
+        for (int next : map.get(cur)) {
+            if(visited[next]){
+                continue;
+            }
+            visited[next] = true;
+
+            int result = solve(next, count + 1);
+            if (result != -1){
+                return result;
+            };
+        }
+        return -1;
+    }
+}
