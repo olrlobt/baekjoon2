b386b4fefcbebec9bdc5277c3976086cc54736a5
Seungheon LEE <317tmdgjs@naver.com>
2023-07-15 22:20:02 +0900

    [Silver I] Title: 줄어들지 않아, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.java
+++ 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.java
@@ -1,17 +1,22 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
 
         for (int testCase = 0; testCase < T; testCase++) {
-            int N = sc.nextInt();
-            System.out.println(solve(N));
+            int N = Integer.parseInt(br.readLine());
+            sb.append(solve(N)).append("\n");
         }
+        System.out.print(sb);
     }
 
     public static long solve(int n){
