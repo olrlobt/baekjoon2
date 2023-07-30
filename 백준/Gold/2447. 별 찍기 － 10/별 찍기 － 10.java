2504a1d4b32ceaa317724258b3402bb853ad3aad
Seungheon LEE <317tmdgjs@naver.com>
2023-07-30 21:14:07 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 312 ms, Memory: 46836 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -6,10 +9,11 @@ public class Main {
     static int N;
     static char[][] map;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
 
-        N = sc.nextInt();
+        N = Integer.parseInt(br.readLine());
         map = new char[N][N];
 
         for (int i = 0; i < N; i++) {
@@ -19,8 +23,9 @@ public class Main {
         solve(N, 0, 0);
 
         for (int i = 0; i < N; i++) {
-            System.out.println(map[i]);
+            sb.append(map[i]).append("\n");
         }
+        System.out.println(sb);
     }
 
     private static void solve(int N, int x, int y) {
