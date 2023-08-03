334c7057a09f2f9f98d3b23a9dbd327b28e0f248
Seungheon LEE <317tmdgjs@naver.com>
2023-08-03 17:01:36 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 616 ms, Memory: 47532 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        sb.append((int)Math.pow(2, N) - 1).append("\n");
+        hanoi(N, 1, 2, 3);
+        System.out.println(sb);
+    }
+
+    private static void hanoi(int N, int start, int mid, int end) {
+
+        if (N == 1) {
+            sb.append(start).append(" ").append(end).append("\n");
+            return;
+        }
+
+        hanoi(N - 1, start, end, mid);
+        sb.append(start).append(" ").append(end).append("\n");
+        hanoi(N - 1, mid, start, end);
+
+    }
+}
