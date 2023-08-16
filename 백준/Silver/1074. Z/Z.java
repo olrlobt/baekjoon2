dbd6bcaca18bd1ef8bf1493efffa371730e336ed
Seungheon LEE <317tmdgjs@naver.com>
2023-08-16 13:43:17 +0900

    [Silver I] Title: Z, Time: 140 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,48 +1,41 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-    static int result = 0;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();  // 2^N x 2^N
-        int R = sc.nextInt();
-        int C = sc.nextInt();
-        solve(N, R, C);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());  // 2^N x 2^N
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        System.out.println(solve(N, R, C, 0));
     }
 
-    public static void solve(int N, int R, int C) { // N = 제곱 // 다음 재귀땐 N -1
-
-
+    public static int solve(int N, int R, int C, int result) { // N = 제곱 // 다음 재귀땐 N -1
+        
         if (N == 0) {
-            System.out.println(result );
-            return;
+            return result;
         }
 
         if (R < Math.pow(2, N - 1)) { // 위 라인
-
-            if (C < Math.pow(2, N - 1)) {//좌측
-
-            } else {
+            if (C >= Math.pow(2, N - 1)) {//좌측
                 result += Math.pow(4, N-1);
                 C -= Math.pow(2, N - 1);
             }
-
         } else {  // 아래 라인
             R -= Math.pow(2, N - 1);
-
             if (C < Math.pow(2, N - 1)) {//좌측
                 result += Math.pow(4, N-1) * 2;
-
             } else {
                 result += Math.pow(4, N-1) * 3;
                 C -= Math.pow(2, N - 1);
             }
         }
-
-        solve(N - 1, R, C);
+        return solve(N - 1, R, C, result);
     }
 }
