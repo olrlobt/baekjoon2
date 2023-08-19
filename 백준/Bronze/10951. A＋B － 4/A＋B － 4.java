1c3702e978debb36137e3d172b067e9f308a2a85
Seungheon LEE <317tmdgjs@naver.com>
2023-08-19 12:20:08 +0900

    [Bronze V] Title: A+B - 4, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        String str;
+
+        while((str=br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(a+b).append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+	}
+}
