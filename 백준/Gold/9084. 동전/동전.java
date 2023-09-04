8434d9c670af07fd43157d99644408228dbff812
Seungheon LEE <317tmdgjs@naver.com>
2023-09-04 11:33:55 +0900

    [Gold V] Title: 동전, Time: 80 ms, Memory: 11708 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -1,44 +1,45 @@
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static List<Integer> unit;
-    static int M;
-    static List<Integer> dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();
+        int T = Integer.parseInt(br.readLine());
 
         for (int testCase = 0; testCase < T; testCase++) {
-            int N = sc.nextInt();
-            dp = new ArrayList<>();
-            unit = new ArrayList<>();
-            for (int i = 0; i < N; i++) {
-                unit.add(sc.nextInt());
-            }
 
-            M = sc.nextInt();
-            solve();
+            int N = Integer.parseInt(br.readLine());
+            int[] coins = new int[N + 1];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int i = 1; i <= N; i++) {
+                coins[i] = Integer.parseInt(st.nextToken());
+            }
+            int goalMoney = Integer.parseInt(br.readLine());
+
+            sb.append(solve(coins, goalMoney)).append("\n");
         }
+        System.out.println(sb);
     }
 
-    public static void solve() {
+    private static int solve(int[] coins, int goalMoney) {
 
-        dp.add(1);
-        for (int i = 0; i < M; i++) {
-            dp.add(0);
-        }
+        int[] dp = new int[goalMoney + 1];
+        dp[0] = 1;
 
-        for (int index = 0; index < unit.size(); index++) {
-            for (int i = unit.get(index); i <= M; i++) {
-                dp.set(i, dp.get(i) + dp.get(i - unit.get(index)));
+        for (int coin = 1; coin < coins.length; coin++) {
+
+            for (int money = coins[coin]; money <= goalMoney; money++) {
+
+                dp[money] += dp[money - coins[coin]];
+
             }
         }
-        System.out.println(dp.get(M));
+        return dp[goalMoney];
     }
 }
