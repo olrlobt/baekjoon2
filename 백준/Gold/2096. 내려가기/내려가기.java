557ea66083345c55f82a28da8a7cac70e42e8a47
Seungheon LEE <317tmdgjs@naver.com>
2023-09-17 23:54:22 +0900

    [Gold V] Title: 내려가기, Time: 324 ms, Memory: 41760 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -5,43 +5,37 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] map;
-    static int[] maxDp;
-    static int[] minDp;
-    static int[] temp;
-    static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][3];
-        temp = new int[3];
-        maxDp = new int[3];
-        minDp = new int[3];
+        int N = Integer.parseInt(br.readLine());
+        
+        int temp = 0;
+        int[] map = new int[3];
+        int[] maxDp = new int[3];
+        int[] minDp = new int[3];
 
         for (int row = 0; row < N; row++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int column = 0; column < 3; column++) {
-                map[row][column] = Integer.parseInt(st.nextToken());
+                map[column] = Integer.parseInt(st.nextToken());
             }
 
-            temp[0] = map[row][0] + Math.max(maxDp[0], maxDp[1]);
-            temp[1] = map[row][1] + Math.max(maxDp[0], Math.max(maxDp[1], maxDp[2]));
-            temp[2] = map[row][2] + Math.max(maxDp[1], maxDp[2]);
+            int max = Math.max(maxDp[1], maxDp[2]);
+            temp = map[0] + Math.max(maxDp[0], maxDp[1]);
+            maxDp[1] = map[1] + Math.max(maxDp[0], max);
+            maxDp[2] = map[2] + max;
 
-            maxDp[0] = temp[0];
-            maxDp[1] = temp[1];
-            maxDp[2] = temp[2];
+            maxDp[0] = temp;
 
-            temp[0] = map[row][0] + Math.min(minDp[0], minDp[1]);
-            temp[1] = map[row][1] + Math.min(minDp[0], Math.min(minDp[1], minDp[2]));
-            temp[2] = map[row][2] + Math.min(minDp[1], minDp[2]);
+            int min = Math.min(minDp[1], minDp[2]);
+            temp = map[0] + Math.min(minDp[0], minDp[1]);
+            minDp[1] = map[1] + Math.min(minDp[0], min);
+            minDp[2] = map[2] + min;
 
-            minDp[0] = temp[0];
-            minDp[1] = temp[1];
-            minDp[2] = temp[2];
+            minDp[0] = temp;
         }
 
         sb.append(Math.max(maxDp[0], Math.max(maxDp[2], maxDp[1]))).append(" ").append(Math.min(minDp[0],
