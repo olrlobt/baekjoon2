8b4566bcd11679603eef9ca2002761b48814fca5
Seungheon LEE <317tmdgjs@naver.com>
2023-09-17 23:30:18 +0900

    [Gold V] Title: 내려가기, Time: 324 ms, Memory: 45056 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,6 +1,7 @@
-
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -10,18 +11,20 @@ public class Main {
     static int[] temp;
     static int N;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        N = sc.nextInt();
+        N = Integer.parseInt(br.readLine());
         map = new int[N][3];
         temp = new int[3];
         maxDp = new int[3];
         minDp = new int[3];
 
         for (int row = 0; row < N; row++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int column = 0; column < 3; column++) {
-                map[row][column] = sc.nextInt();
+                map[row][column] = Integer.parseInt(st.nextToken());
             }
 
             temp[0] = map[row][0] + Math.max(maxDp[0], maxDp[1]);
@@ -41,9 +44,8 @@ public class Main {
             minDp[2] = temp[2];
         }
 
-        System.out.println(Math.max(maxDp[0], Math.max(maxDp[2], maxDp[1])) + " " + Math.min(minDp[0], Math.min(minDp[2], minDp[1])));
-
-
-
+        sb.append(Math.max(maxDp[0], Math.max(maxDp[2], maxDp[1]))).append(" ").append(Math.min(minDp[0],
+                Math.min(minDp[2], minDp[1])));
+        System.out.println(sb);
     }
 }
