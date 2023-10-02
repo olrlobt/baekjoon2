42c672488d0b53911930742fbfd27a068556fb9a
Seungheon LEE <317tmdgjs@naver.com>
2023-10-02 23:54:52 +0900

    sloved 최대 곱.java

---
+++ 백준/Silver/1500. 최대 곱/최대 곱.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int S = sc.nextInt(); // 정수 S
+		int K = sc.nextInt(); // 합이 S인 K개의 정수
+		int div = S / K; 
+		int mod = S % K;
+		long max = 1;
+		for (int i=1; i<=K; i++) {
+			if(i<=mod) { // 나머지 갯수만큼 +1
+				max *= (div+1);
+			}else {
+				max *= div;
+			}
+		
+		}
+		System.out.println(max);
+	}	
+}
