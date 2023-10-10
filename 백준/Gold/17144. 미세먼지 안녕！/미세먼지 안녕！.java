3b0f5beb33602b75b9eb160116fac522e63ba4ef
Seungheon LEE <317tmdgjs@naver.com>
2023-10-10 14:44:56 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 288 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static final int[] dx = {0, -1, 0, 1};
+    static final int[] dy = {-1, 0, 1, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken()); // 초
+
+        int[][] map = new int[R][C];
+        int sum = 2;
+        for (int row = 0; row < R; row++) {
+            st = new StringTokenizer(br.readLine());
+            for (int column = 0; column < C; column++) {
+                map[row][column] = Integer.parseInt(st.nextToken());
+                sum += map[row][column];
+            }
+        }
+
+        System.out.println(sum - solve(map, T));
+    }
+
+
+    private static int solve(int[][] map, int t) {
+
+        int clean = 0;
+        int cleanerRow = 0;
+        for (int row = 0; row < map.length; row++) {
+            if (map[row][0] == -1) {
+                cleanerRow = row;
+                break;
+            }
+        }
+
+        int[][] tempMap = new int[map.length][map[0].length];
+
+        while (t-- > 0) {
+
+            for (int row = 0; row < map.length; row++) {
+                Arrays.fill(tempMap[row], 0);
+            }
+
+            for (int row = 0; row < map.length; row++) {
+                for (int column = 0; column < map[0].length; column++) {
+                    if (map[row][column] <= 0) {
+                        continue;
+                    }
+                    int count = 0;
+
+                    for (int i = 0; i < 4; i++) {
+                        int nextRow = row + dy[i];
+                        int nextColumn = column + dx[i];
+
+                        if (nextRow >= map.length || nextRow < 0 || nextColumn >= map[0].length || nextColumn < 0
+                                || (nextColumn == 0 && (nextRow == cleanerRow || nextRow == cleanerRow + 1))) {
+                            continue;
+                        }
+                        count++;
+                        tempMap[nextRow][nextColumn] += map[row][column] / 5;
+                    }
+                    tempMap[row][column] += map[row][column] - map[row][column] / 5 * count;
+                }
+            }
+
+            // 상단
+            System.arraycopy(tempMap[0], 1, map[0], 0, map[0].length - 1);
+            System.arraycopy(tempMap[cleanerRow], 1, map[cleanerRow], 2, map[0].length - 2);
+
+            for (int row = 1; row < cleanerRow; row++) {
+                map[row][0] = tempMap[row - 1][0];
+                map[row][map[0].length - 1] = tempMap[row + 1][map[0].length - 1];
+            }
+            clean += tempMap[cleanerRow - 1][0];
+            map[0][map[0].length - 1] = tempMap[1][map[0].length - 1];
+            map[cleanerRow][1] = 0;
+
+            //하단
+            System.arraycopy(tempMap[cleanerRow + 1], 1, map[cleanerRow + 1], 2, map[0].length - 2);
+            System.arraycopy(tempMap[map.length - 1], 1, map[map.length - 1], 0, map[0].length - 1);
+
+            for (int row = cleanerRow + 2; row < map.length - 1; row++) {
+                map[row][map[0].length - 1] = tempMap[row - 1][map[0].length - 1];
+                map[row][0] = tempMap[row + 1][0];
+            }
+
+            clean += tempMap[cleanerRow + 2][0];
+            map[map.length - 1][map[0].length - 1] = tempMap[map.length - 2][map[0].length - 1];
+            map[cleanerRow + 1][1] = 0;
+
+
+            for (int row = 1; row < cleanerRow; row++) {
+                System.arraycopy(tempMap[row], 1, map[row], 1, map[0].length - 2);
+            }
+            for (int row = cleanerRow + 2; row < map.length - 1; row++) {
+                System.arraycopy(tempMap[row], 1, map[row], 1, map[0].length - 2);
+            }
+        }
+
+        return clean;
+    }
+}
