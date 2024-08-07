504b8c1fd277ed296ed2de1e8eb330f6089e9904
Seungheon LEE <317tmdgjs@naver.com>
2024-08-07 21:30:41 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 11768 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,39 +1,39 @@
-
-import java.lang.reflect.Array;
-import java.rmi.UnexpectedException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static int[][] map;
     static boolean[][] visited;
     static int N;
-    static int [] dx = {1,0,-1,0};
-    static int [] dy = {0,-1,0,1};
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
 
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         visited = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
-            String input = sc.next();
+            st = new StringTokenizer(br.readLine());
+            String input = st.nextToken();
 
             for (int j = 0; j < N; j++) {
                 map[i][j] = input.charAt(j) - '0';
             }
-
         }
-
-        int count = 1;
+        int count = 0;
         PriorityQueue<Integer> result = new PriorityQueue<>();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -45,48 +45,38 @@ public class Main {
 
             }
         }
-
-        System.out.println(count - 1); // 총 단지수
+        sb.append(count).append('\n');
         while (!result.isEmpty()){
-            System.out.println(result.poll());
+            sb.append(result.poll()).append('\n');
         }
+        System.out.println(sb);
     }
 
     public static int solve(int row, int column) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{row, column});
+        int count = 1;
+        visited[row][column] = true;
 
-        Queue<int []> queue = new LinkedList<>();
-        queue.offer(new int []{ row,column});
-        int count = 0 ;
+        while (!queue.isEmpty()) {
 
-        while (!queue.isEmpty()){
-
-            int [] cur = queue.poll();
+            int[] cur = queue.poll();
             int curRow = cur[0];
             int curColumn = cur[1];
 
-            if(visited[curRow][curColumn]){
-                continue;
-            }
-            visited[curRow][curColumn] = true;
-            count ++;
-
-            for(int i = 0 ; i < 4; i ++){
-
+            for (int i = 0; i < 4; i++) {
                 int nextRow = curRow + dy[i];
                 int nextColumn = curColumn + dx[i];
 
-                if(nextColumn < 0 || nextRow < 0 || nextRow >= N || nextColumn >= N){
+                if (nextColumn < 0 || nextRow < 0 || nextRow >= N || nextColumn >= N || map[nextRow][nextColumn] == 0
+                || visited[nextRow][nextColumn]) {
                     continue;
                 }
-                if(map[nextRow][nextColumn] == 0){
-                    continue;
-                }
-
-                queue.offer(new int[] { nextRow, nextColumn});
+                visited[nextRow][nextColumn] = true;
+                count++;
+                queue.offer(new int[]{nextRow, nextColumn});
             }
         }
-
         return count;
-
     }
 }
