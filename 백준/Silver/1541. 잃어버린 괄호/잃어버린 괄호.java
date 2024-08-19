3f5ea9d5f9b634e3a452384e9e7ab9385588dfa4
Seungheon LEE <317tmdgjs@naver.com>
2024-08-19 23:04:52 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 64 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,31 +1,28 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int sum = Integer.MAX_VALUE;	
-		String[] subtraction = in.nextLine().split("-");
-		
- 
-		for(int i = 0; i < subtraction.length; i++) {
-			int temp = 0;
- 
-			String[] addition = subtraction[i].split("\\+");
-			
-			for(int j = 0; j < addition.length; j++) {
-				temp += Integer.parseInt(addition[j]);
-			}
-			
-			if (sum == Integer.MAX_VALUE) {
-				sum = temp;
-			} else {
-				sum -= temp;
-			}
-		}
-		System.out.println(sum);
-	}
- 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = Integer.MAX_VALUE;
+        int temp = 0;
+        StringTokenizer subtraction = new StringTokenizer(br.readLine(), "-");
+        StringTokenizer addition;
+        while (subtraction.hasMoreTokens()) {
+            temp = 0;
+
+            addition = new StringTokenizer(subtraction.nextToken(), "+");
+            while (addition.hasMoreTokens()) {
+                temp += Integer.parseInt(addition.nextToken());
+            }
+            if (sum == Integer.MAX_VALUE) {
+                sum = temp;
+            } else {
+                sum -= temp;
+            }
+        }
+        System.out.println(sum);
+    }
 }
