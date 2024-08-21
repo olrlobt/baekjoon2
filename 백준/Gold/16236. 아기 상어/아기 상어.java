40416fc7c6304f1a0eda9af419b7562b40942084
Seungheon LEE <317tmdgjs@naver.com>
2024-08-21 18:12:09 +0900

    [Gold III] Title: 아기 상어, Time: 76 ms, Memory: 11840 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,151 +1,120 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static int[][] map;
-    static boolean[][] target;
-    static int result = 0;
-    static int targetCount = 0;
-    static int[] shark;
-    static int size = 2;
-    static int eat = 0;
     static int[] dx = {0, -1, 1, 0}; // 상 좌 우 하
     static int[] dy = {-1, 0, 0, 1};
+    static boolean[][] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-        map = new int[N][N];
-        target = new boolean[N][N];
+        int N = Integer.parseInt(br.readLine());
+        Shark[][] map = new Shark[N][N];
+        visited = new boolean[N][N];
+        BabyShark babyShark = null;
 
         for (int row = 0; row < N; row++) {
+            st = new StringTokenizer(br.readLine());
             for (int column = 0; column < N; column++) {
-                int input = sc.nextInt();
-                map[row][column] = input;
-
-                if (input == 9) {
-                    map[row][column] = 0;
-                    shark = new int[]{row, column};
-                } else if (input == 1) {
-                    target[row][column] = true;
-                    targetCount++;
-                }
-            }
-        }
-        solve();
-        System.out.println(result);
-    }
-
-
-    public static void solve() {
-        while (targetCount != 0) {
-
-            int[] fish = distance();
-            if(fish == null){
-                break;
-            }
-
-            move(fish);
-        }
-    }
-
-    public static void move(int[] fish) {
-
-        map[fish[0]][fish[1]] = 0;
-        target[fish[0]][fish[1]] = false;
-        targetCount--;
-        eat++;
-
-        if (eat == size) {
-            eat = 0;
-            size++;
-            targetAdd();
-        }
-        shark = fish;
-    }
-
-    private static void targetAdd() {
-        for (int row = 0; row < N; row++) {
-            for (int column = 0; column < N; column++) {
-                if (map[row][column] == size - 1) {
-                    target[row][column] = true;
-                    targetCount++;
-                }
-            }
-        }
-
-    }
-
-
-    public static int[] distance() {
-
-        Queue<int[]> visited = new LinkedList<>();
-        int[][] distanceMap = new int[N][N];
-
-        List<int[]> targetList = new ArrayList<>();
-
-        visited.add(shark);
-
-        while (!visited.isEmpty()) {
-
-            int[] position = visited.poll();
-            int row = position[0];
-            int column = position[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nextRow = row + dy[i];
-                int nextColumn = column + dx[i];
-
-                if (nextRow < 0 || nextRow >= N || nextColumn < 0 || nextColumn >= N) {
+                int input = Integer.parseInt(st.nextToken());
+                if (input == 0) {
+                    continue;
+                } else if (input == 9) {
+                    babyShark = new BabyShark(row, column, 2, 0, 0);
                     continue;
                 }
-
-                if (map[nextRow][nextColumn] <= size && distanceMap[nextRow][nextColumn] == 0) {
-
-                    if (target[nextRow][nextColumn]) {
-                        targetList.add(new int[]{nextRow, nextColumn});
-                    }
-
-                    distanceMap[nextRow][nextColumn] = distanceMap[row][column] + 1;
-
-                    if (targetList.isEmpty()) {
-                        visited.offer(new int[]{nextRow, nextColumn});
-                    }
-                }
+                map[row][column] = new Shark(row, column, input);
             }
         }
 
-
-        if(targetList.isEmpty()){
-            return null;
-        }
-        Collections.sort(targetList, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (distanceMap[o1[0]][o1[1]] == distanceMap[o2[0]][o2[1]]) {
-                    if (o1[0] == o2[0]) {
-                        return o1[1] - o2[1];
-                    }
-                    return o1[0] - o2[0];
-                } else {
-
-                    return distanceMap[o1[0]][o1[1]] - distanceMap[o2[0]][o2[1]];
-                }
-            }
-        });
-
-
-        result += distanceMap[targetList.get(0)[0]][targetList.get(0)[1]];
-        return targetList.get(0);
+        System.out.println(solve(map, babyShark));
     }
 
+    private static int solve(Shark[][] map, BabyShark shark) {
+        PriorityQueue<BabyShark> pqq = new PriorityQueue<>();
+        pqq.offer(shark);
+        visited[shark.row][shark.col] = true;
+        int result = 0;
 
+        while (!pqq.isEmpty()) {
+            BabyShark curBaby = pqq.poll();
+
+            if(map[curBaby.row][curBaby.col] != null &&  map[curBaby.row][curBaby.col].size < curBaby.size) {
+                pqq.clear();
+                map[curBaby.row][curBaby.col] = null;
+                curBaby.eat();
+
+                clearVisited(curBaby);
+                result = curBaby.result;
+            }
+
+            for (int idx = 0; idx < 4; idx++) {
+                int nextRow = curBaby.row + dy[idx];
+                int nextCol = curBaby.col + dx[idx];
+
+                if(nextRow < 0 || nextCol < 0 || nextRow >= map.length || nextCol >= map[0].length || visited[nextRow][nextCol]
+                || (map[nextRow][nextCol] != null && map[nextRow][nextCol].size > curBaby.size)) {
+                    continue;
+                }
+                visited[nextRow][nextCol] = true;
+                pqq.offer(new BabyShark(nextRow, nextCol, curBaby.size, curBaby.eat, curBaby.result + 1));
+            }
+        }
+        return result;
+    }
+
+    private static void clearVisited(BabyShark babyShark){
+        for (int row = 0; row < visited.length; row++) {
+            Arrays.fill(visited[row], false);
+        }
+        visited[babyShark.row][babyShark.col] = true;
+    }
+
+    private static class BabyShark extends Shark implements Comparable<BabyShark>{
+        int eat;
+        int result;
+
+        public BabyShark(int row, int col, int size, int eat, int result) {
+            super(row, col, size);
+            this.eat = eat;
+            this.result = result;
+        }
+
+        @Override
+        public int compareTo(BabyShark o) {
+            if(result == o.result){
+                if(this.row == o.row){
+                    return this.col - o.col;
+                }
+                return this.row - o.row;
+            }
+            return this.result - o.result;
+        }
+
+        public void eat() {
+            this.eat++;
+            if(this.eat == this.size){
+                this.eat = 0;
+                this.size++;
+            }
+        }
+    }
+
+    private static class Shark {
+        int row;
+        int col;
+        int size;
+
+        public Shark(int row, int col, int size) {
+            this.row = row;
+            this.col = col;
+            this.size = size;
+        }
+    }
 }
