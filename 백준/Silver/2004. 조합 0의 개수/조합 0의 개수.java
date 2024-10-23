4c71d10723e1261c07d7a4294c851eff4704329b
Seungheon LEE <317tmdgjs@naver.com>
2024-10-23 23:35:02 +0900

    [Silver II] Title: 조합 0의 개수, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+ 
+		long N = Long.parseLong(st.nextToken());
+		long M = Long.parseLong(st.nextToken());
+ 
+		long count5 = five_power_n(N) - five_power_n(N - M) - five_power_n(M);
+		long count2 = two_power_n(N) - two_power_n(N - M) - two_power_n(M);
+		System.out.println(Math.min(count5, count2));
+ 
+	}
+ 
+	static long five_power_n(long num) {
+		int count = 0;
+ 
+		while (num >= 5) {
+			count += (num / 5);
+			num /= 5;
+		}
+		return count;
+	}
+ 
+	static long two_power_n(long num) {
+		int count = 0;
+ 
+		while (num >= 2) {
+			count += (num / 2);
+			num /= 2;
+		}
+		return count;
+	}
+ 
+}
